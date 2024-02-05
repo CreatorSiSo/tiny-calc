e65102a93e4b7bc7d017b25413544e5ce5caed3a
Simon Sommer <64036709+CreatorSiSo@users.noreply.github.com>
2024-02-05 21:24:59 +0100

    Remove functions that I would have to document.

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,8 @@
 #include "print.hpp"
 #include "repl.hpp"
 
-static void write_usage(std::ostream& out) {
-    write(
-        out,
+auto main(int argc, char* argv[]) -> int {
+    constexpr auto USAGE =
         "Usage:\n"
         "  tiny-calc [OPTIONS]\n"
         "\n"
@@ -15,19 +14,8 @@ static void write_usage(std::ostream& out) {
         "  --plain            Only print the results of the calculation,\n"
         "                     or error messages (useful for piping)\n"
         "  --print-tokens     Print token streams\n"
-        "  --print-chunks     Print compiled chunks\n"
-    );
-}
+        "  --print-chunks     Print compiled chunks\n";
 
-static void write_help(std::ostream& out) {
-    writeln(
-        out,
-        "A very minimal terminal calculator with a bit of an unusual syntax.\n"
-    );
-    write_usage(out);
-}
-
-auto main(int argc, char* argv[]) -> int {
     std::istream& in = std::cin;
     std::ostream& out = std::cout;
 
@@ -40,7 +28,12 @@ auto main(int argc, char* argv[]) -> int {
     const auto args = std::vector<std::string>(argv, argv + argc);
     for (const auto& arg : args | std::views::drop(1)) {
         if (arg == "-h" || arg == "--help" || arg == "-?") {
-            write_help(out);
+            writeln(
+                out,
+                "A very minimal terminal calculator with a bit of an unusual "
+                "syntax."
+            );
+            write(out, "\n{}", USAGE);
             exit(0);
         } else if (arg == "--plain") {
             config.plain = true;
@@ -49,8 +42,8 @@ auto main(int argc, char* argv[]) -> int {
         } else if (arg == "--print-chunks") {
             config.print_chunks = true;
         } else {
-            writeln(out, "Error: Invalid argument '{}'\n", arg);
-            write_usage(out);
+            writeln(out, "Error: Invalid argument '{}'", arg);
+            write(out, "\n{}", USAGE);
             exit(-1);
         }
     }
